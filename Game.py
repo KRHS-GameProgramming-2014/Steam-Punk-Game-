4d90360294ac4db7996f5187ddb6114b8a1b4cc9
Majarky <myles.ward@kearsarge.org>
2015-04-09 10:40:10 -0400

    With our start screen image

--- Game.py
+++ Game.py
@@ -16,7 +16,7 @@ bgColor = r,g,b = 0, 0, 10
 
 screen = pygame.display.set_mode(size)
 
-bgImage = pygame.image.load("RS/Main Menu/Start Screen.png").convert()
+bgImage = pygame.image.load("RS/Main Menu/Startscreen.png").convert()
 bgRect = bgImage.get_rect()
 
 backgrounds = pygame.sprite.Group()
@@ -28,7 +28,7 @@ Block.containers = (all, blocks)
 
 run = False
 
-startButton = Button([width/2, height-300], 
+startButton = Button([width/2, height-200], 
 				     "RS/Main Menu/Start Base.png", 
 				     "RS/Main Menu/Start Clicked.png")
 
