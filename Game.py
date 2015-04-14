9633c0cc54fa7e8328e73d4667fd8b87dcf7b239
Majarky <myles.ward@kearsarge.org>
2015-04-14 13:09:18 -0400

    I forget what I was working on

--- Game.py
+++ Game.py
@@ -15,7 +15,7 @@ size = width, height
 bgColor = r,g,b = 0, 0, 10
 
 screen = pygame.display.set_mode(size)
-#fullscreen = False
+fullscreen = False
 
 bgImage = pygame.image.load("RS/Main Menu/Startscreen.png").convert()
 bgRect = bgImage.get_rect()
@@ -30,46 +30,64 @@ Block.containers = (all, blocks)
 run = False
 
 startButton = Button([width/2, height-200], 
-				     "RS/Main Menu/Start Base.png", 
-				     "RS/Main Menu/Start Clicked.png")
+                     "RS/Main Menu/Start Base.png", 
+                     "RS/Main Menu/Start Clicked.png")
 
 while True:
-	while not run:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: sys.exit()
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RETURN:
-					run = True
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				startButton.click(event.pos)
-			if event.type == pygame.MOUSEBUTTONUP:
-				if startButton.release(event.pos):
-					run = True
-					
-		bgColor = r,g,b
-		screen.fill(bgColor)
-		screen.blit(bgImage, bgRect)
-		screen.blit(startButton.image, startButton.rect)
-		pygame.display.flip()
-		clock.tick(60)
-		
-	BackGround("RS/background.png")
+    while not run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    run = True
+                if event.key == pygame.K_RSHIFT:
+                    print event.mod, pygame.KMOD_RALT
+                    if event.mod & pygame.KMOD_RALT: #Binary and with KMOD_RIGHT to filter out other mod keys
+                        if fullscreen:
+                            pygame.display.set_mode(size)
+                            fullscreen = True
+                        else:
+                            pygame.display.set_mode(size, pygame.FULLSCREEN)
+                            fullscreen = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                startButton.click(event.pos)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if startButton.release(event.pos):
+                    run = True
+                    
+        bgColor = r,g,b
+        screen.fill(bgColor)
+        screen.blit(bgImage, bgRect)
+        screen.blit(startButton.image, startButton.rect)
+        pygame.display.flip()
+        clock.tick(60)
+        
+    BackGround("RS/background.png")
 
-	while run:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: sys.exit()
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_w or event.key == pygame.K_UP:
-					player.go("up")
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_w or event.key == pygame.K_UP:
-					player.go("stop up")
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    player.go("up")
+            if event.key == pygame.K_RSHIFT:
+                    print event.mod, pygame.KMOD_RALT
+                    if event.mod & pygame.KMOD_RALT: #Binary and with KMOD_RIGHT to filter out other mod keys
+                        if fullscreen:
+                            pygame.display.set_mode(size)
+                            fullscreen = True
+                        else:
+                            pygame.display.set_mode(size, pygame.FULLSCREEN)
+                            fullscreen = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    player.go("stop up")
 
 
-		all.update(width, height)
-		
-		dirty = all.draw(screen)
-		pygame.display.update(dirty)
-		pygame.display.flip()
-		clock.tick(60)
-		
+        all.update(width, height)
+        
+        dirty = all.draw(screen)
+        pygame.display.update(dirty)
+        pygame.display.flip()
+        clock.tick(60)
+        
